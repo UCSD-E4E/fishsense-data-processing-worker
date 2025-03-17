ae1f61e73dfe5cb47dce9737332e49c88d8d4ee1
Nathan Hui <nthui@ucsd.edu>
2025-03-17 01:03:05 -0700

    fix: Adds logging

--- fishsense_data_processing_worker/service.py
+++ fishsense_data_processing_worker/service.py
@@ -1,7 +1,10 @@
 '''Fishsense Data Processing Worker
 '''
+from threading import Event
+
 from prometheus_client import start_http_server
 
+from fishsense_data_processing_worker.config import configure_logging
 from fishsense_data_processing_worker.metrics import system_monitor_thread
 
 
@@ -11,15 +14,15 @@ class Service:
     # pylint: disable=too-few-public-methods
     # runtime
     def __init__(self):
-        pass
+        self.stop_event = Event()
+        configure_logging()
 
     def run(self):
         """Main entry point
         """
         start_http_server(9090)
         system_monitor_thread.start()
-        while True:
-            pass
+        self.stop_event.wait()
 
 
 def main() -> None:
